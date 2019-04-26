89afd466cfa17ad1500ff88655c3f1eaddeef794
alex <you@example.com>
2019-04-26 17:40:30 +0000

    updated routes and created a cookie session

--- server.js
+++ server.js
@@ -13,9 +13,11 @@ const knexConfig  = require("./knexfile");
 const knex        = require("knex")(knexConfig[ENV]);
 const morgan      = require('morgan');
 const knexLogger  = require('knex-logger');
+const cookieSession = require('cookie-session')
 
 // Seperated Routes for each Resource
 const itemsRoutes = require("./routes/items");
+const ordersRoutes = require("./routes/orders")
 
 // Load the logger first so all (static) HTTP requests are logged to STDOUT
 // 'dev' = Concise output colored by response status for development use.
@@ -25,7 +27,6 @@ app.use(morgan('dev'));
 // Log knex SQL queries to STDOUT as well
 app.use(knexLogger(knex));
 
-app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use("/styles", sass({
   src: __dirname + "/styles",
@@ -34,11 +35,29 @@ app.use("/styles", sass({
   outputStyle: 'expanded'
 }));
 app.use(express.static("public"));
+app.use(cookieSession({
+  name: 'session',
+  keys: ['key1', 'key2'],
+  maxAge: 24 * 60 * 60 * 1000 // 24 hours
+}));
+app.set("view engine", "ejs");
+
+// <---------Functions---------->
+
+function generateRandomString() {
+  var text = "";
+  var possible = "01234567";
+  for (var i = 0; i < 7; i++) {
+    text += possible.charAt(Math.floor(Math.random() * possible.length))
+  }
+  return parseInt(text);
+}
 
 // <-------------GET ROUTES------------------>
 
 // Mount all resource routes
 app.use("/api/menu", itemsRoutes(knex));
+app.use("/api/order", itemsR )
 
 // Home page
 app.get("/", (req, res) => {
@@ -67,8 +86,18 @@ app.get("/menu/create-your-own", (req, res) => {
 
 // <--------POST ROUTES---------->
 
+app.post("/", (req, res) => {
+  let cookie = generateRandomString();
+  req.session.user = cookie;
+res.redirect("/menu");
+});
+
 app.post("/menu", (req, res) => {
-  res.redirect("/menu");
+
+  if (req.session.user) {
+    let id = generateRandomString();
+  }
+  res.redirect("/order");
 });
 
 app.post("/order", (req, res) => {
